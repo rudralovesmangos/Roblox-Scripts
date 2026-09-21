af68b6fb2149ab63a745dcb67608a5d3b76e2eae
rudralovesmangos <rudralovesmangos21@gmail.com>
2026-09-21 14:36:30 +0530

    Add hug-remote-event.lua for hug request functionality

---
+++ Troll-Hug-Tower/stuff/hug-remote-event.lua
@@ -0,0 +1,6 @@
+local args = {
+    [1] = "tryGrab",
+    [2] = game:GetService("Players").PogiAngelo021129.Character
+}
+
+game:GetService("ReplicatedStorage").HugRemotes.HugRequest:FireServer(unpack(args))
